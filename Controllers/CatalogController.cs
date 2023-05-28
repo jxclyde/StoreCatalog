c36497b2d595b5077a0bfb54c5ea1cd501390dfd
Roman Onyshkiv <roman.onushkiv@gmail.com>
2023-05-28 21:00:09 +0300

    Creating CatalogController. Adding Model. Editing views and some css

---
+++ Controllers/CatalogController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StoreCatalog.Data;
+using StoreCatalog.Models;
+
+namespace StoreCatalog.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CatalogController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var processors = _db.processors.ToList();
+            var motherboards = _db.motherboards.ToList();
+            var graphicsCards = _db.graphicscards.ToList();
+
+            var catalogViewModel = new CatalogViewModel
+            {
+                Processors = processors,
+                Motherboards = motherboards,
+                GraphicsCards = graphicsCards
+            };
+
+            return View(catalogViewModel);
+        }
+    }
+}
---
+++ Models/CatalogViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace StoreCatalog.Models
+{
+    public class CatalogViewModel
+    {
+        public List<Processor> Processors { get; set; }
+        public List<Motherboard> Motherboards { get; set; }
+        public List<GraphicsCard> GraphicsCards { get; set; }
+    }
+}
