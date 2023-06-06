e271c323cb8393673ce851c02f63af515fb645bf
Roman Onyshkiv <roman.onushkiv@gmail.com>
2023-06-07 01:06:48 +0300

    Changes will be added. MVC structure to be redesigned :/

--- Controllers/CatalogController.cs
+++ Controllers/CatalogController.cs
@@ -46,5 +46,25 @@ namespace StoreCatalog.Controllers
 
             return View(catalogViewModel);
         }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(CatalogViewModel catalogViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                // Save changes to the processors, motherboards, and graphics cards in the database
+                _db.processors.AddRange(catalogViewModel.Processors);
+                _db.motherboards.AddRange(catalogViewModel.Motherboards);
+                _db.graphicscards.AddRange(catalogViewModel.GraphicsCards);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index"); // Redirect to the index page or any other desired page
+            }
+
+            // If the model state is not valid, return the view with the same catalogViewModel to display validation errors
+            return View(catalogViewModel);
+        }
     }
 }
